2d15c59c9f56feb5be5339e505aa9fdca96d4a52
Tanyaa <tanyaadahiya0@gmail.com>
2024-04-11 01:22:35 +0530

    by using viewBinding set layout and change text

--- app/src/main/java/com/snappy/notesapp/MainActivity.kt
+++ app/src/main/java/com/snappy/notesapp/MainActivity.kt
@@ -5,16 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.snappy.notesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding=ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        binding.text.text="hye!"
     }
+
 }
